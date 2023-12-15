5ed7db2c11f7a1d1873e5f18fb63de1477fa880b
zhaoyingkun <yingkun.zhao@mxplayer.in>
2023-12-15 15:29:50 +0800

    delete uesless code

--- learnGradle_7.x/plugin/src/main/java/com/yocn/plugin/CustomPlugin.java
+++ learnGradle_7.x/plugin/src/main/java/com/yocn/plugin/CustomPlugin.java
@@ -6,6 +6,6 @@ import org.gradle.api.Project;
 public class CustomPlugin implements Plugin<Project> {
     @Override
     public void apply(Project target) {
-        System.out.println("自定义的Plugin    CustomPlugin apply: $target");
+        System.out.println("自定义的Plugin    CustomPlugin apply: " + target);
     }
 }
