758f3e318a0c79d18cf5177b96a8b690616c512c
zhaoyingkun <yingkun.zhao@mxplayer.in>
2023-12-15 17:20:00 +0800

    gradle 8 version work

--- learnGradle_8.x/module/src/main/java/com/yocn/plugin/CustomPlugin.java
+++ learnGradle_8.x/module/src/main/java/com/yocn/plugin/CustomPlugin.java
@@ -6,6 +6,6 @@ import org.gradle.api.Project;
 public class CustomPlugin implements Plugin<Project> {
     @Override
     public void apply(Project target) {
-        System.out.println("CustomPlugin apply: $target");
+        System.out.println("自定义的CustomPlugin apply: $target");
     }
 }
